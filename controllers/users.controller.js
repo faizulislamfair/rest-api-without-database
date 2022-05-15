27fbe116249f7856ded13c2001d643a2c327b77f
faizulislamfair <faizulislamfair@gmail.com>
2022-05-15 12:12:54 +0600

    rest api without database

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,11 +1,46 @@
-const users = require('../models/users.model')
+let users = require('../models/users.model') // let na dile push korte parbona
+const { v4: uuidv4 } = require('uuid')
 
+
+// get users
 const getAllUsers = (req, res)=>{
     res.status(200).json({ users });
 };
 
+
+// create users
 const createUser = (req, res)=>{
-    res.status(200).json({ message: "This is a post method" });
+    const newUser = {
+        id: uuidv4(),
+        username: req.body.username,
+        email: req.body.email
+    }
+    users.push(newUser)
+    res.status(201).json(users);
 };
 
-module.exports = { getAllUsers, createUser };
+
+// update user
+const updateUser = (req, res)=>{
+    const userid = req.params.id;
+    const {username, email} = req.body;
+    users
+        .filter((user) => user.id === userid)
+        .map((selecteduser)=>{
+        selecteduser.username = username;
+        selecteduser.email = email;
+    })
+    res.status(200).json(users);
+};
+
+
+// delete user
+const deleteUser = (req, res)=>{
+    const deleteid = req.params.id;
+    users = users.filter((user)=> user.id !== deleteid)
+    res.status(200).json(users);
+};
+
+
+
+module.exports = { getAllUsers, createUser, updateUser, deleteUser };
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,7 +1,9 @@
-const { getAllUsers, createUser } = require('../controllers/users.controller')
+const { getAllUsers, createUser, updateUser, deleteUser } = require('../controllers/users.controller')
 const router = require('express').Router()
 
 router.get("/", getAllUsers );
 router.post("/", createUser );
+router.put("/:id", updateUser );
+router.delete("/:id", deleteUser );
 
 module.exports = router;
